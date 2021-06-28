c7632f4aef65984d20c1865eab093ea5747e6383
analiticofx <79728365+analiticofx@users.noreply.github.com>
2021-06-27 20:56:29 -0400

    updated tests and Token.sol

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,17 +1,33 @@
+
+
 pragma solidity ^0.5.0;
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract Token {
+    using SafeMath for uint;
+
     string public name = "NOVA Token";
     string public symbol = 'NOVA';
     uint256 public decimals = 18;
     uint256 public totalSupply;
-
-    //track balances
     mapping(address => uint256) public balanceOf;
-    //send tokens
+
+    //Events
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
     constructor() public {
-        totalSupply  = 1000000000000000000000000000000;
+        totalSupply  = 1000000000000 * (10 ** decimals);
+        balanceOf[msg.sender] = totalSupply;
     }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+        balanceOf[_to] = balanceOf[_to].add(_value);
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+
 
 }
--- test/Token.test.js
+++ test/Token.test.js
@@ -1,17 +1,18 @@
 
 
+import { tokens } from './helpers'
+import { EVM_REVERT } from './helpers'
 const Token = artifacts.require('./Token')
-
 require('chai')
     .use(require('chai-as-promised'))
     .should()
 
-contract('Token', (accounts) => {
+contract('Token', ([deployer, receiver]) => {
     const name = 'NOVA Token'
     const symbol = 'NOVA'
     const decimals = '18'
-    const totalSupply = '1000000000000000000000000000000'
-    let token
+    const totalSupply = tokens(1000000000000).toString()
+    let token = 0;
 
     beforeEach(async() => {
         token = await Token.new() 
@@ -33,10 +34,61 @@ contract('Token', (accounts) => {
             result.toString().should.equal(decimals)
         })
 
-        it('tracks the total sypply', async() => {
+        it('tracks the total supply', async() => {
             const result = await token.totalSupply()
-            result.toString().should.equal(totalSupply)
+            result.toString().should.equal(totalSupply.toString())
+        })
+
+        it('assigns the total supply to the deployer', async() => {
+            const result = await token.balanceOf(deployer)
+            result.toString().should.equal(totalSupply.toString())
         })
     })
-})
 
+    describe('sending tokens', () => {
+        let amount = '';
+        let result = '';
+        
+        describe('success', async () => {
+            beforeEach(async () => {
+                amount = tokens(100)
+                result = await token.transfer(receiver, amount, { from: deployer })
+                })
+                
+            it('transfers token balances', async () => {
+                let balanceOf = await token.balanceOf(deployer)
+                balanceOf.toString().should.equal(tokens(999999999900).toString())
+                balanceOf = await token.balanceOf(receiver)
+                balanceOf.toString().should.equal(tokens(100).toString())
+            })
+
+            it('emits a Transfer event', () => {
+                const log = result.logs[0]
+                log.event.should.eq('Transfer')
+                const event = log.args
+                event.from.toString().should.equal(deployer, 'from is correct')
+                event.to.should.equal(receiver, 'to is correct')
+                event.value.toString().should.equal(amount.toString(), 'value is correct')
+            })
+        })
+
+        describe('failure', async () => {
+
+            it('rejects insufficient balances', async () => {
+                
+                let invalidAmount = tokens(100000000000000000)//more than the current supply
+                await token.transfer(receiver, invalidAmount, { from: deployer }).should.be.rejectedWith(EVM_REVERT)
+            })
+
+            // it('emits a Transfer event', () => {
+            //     const log = result.logs[0]
+            //     log.event.should.eq('Transfer')
+            //     const event = log.args
+            //     event.from.toString().should.equal(deployer, 'from is correct')
+            //     event.to.should.equal(receiver, 'to is correct')
+            //     event.value.toString().should.equal(amount.toString(), 'value is correct')
+            // })
+        })
+
+    })
+})
